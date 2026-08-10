e5bdeafe246787d280ff025024651cdf28d8fd12
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-08-11 00:31:28 +0700

    feat(v3-clean): export provider execution API

--- v3/research_os_v3/__init__.py
+++ v3/research_os_v3/__init__.py
@@ -3,11 +3,23 @@ from .contracts import health_contract, master_contract, providers_contract
 from .factory import SoftwareFactory
 from .models import OrchestrationDecision, ScaleProfile, ScaleTier, Workload
 from .orchestrator import UnifiedMasterOrchestrator
-from .providers import MockProvider, ProviderRegistry, ProviderStatus
+from .providers import (
+    CompletionRequest,
+    CompletionResponse,
+    MockProvider,
+    OpenAICompatibleProvider,
+    ProviderRegistry,
+    ProviderStatus,
+)
+from .secrets import EnvironmentSecretSource
 
 __all__ = [
     "BrainCore",
+    "CompletionRequest",
+    "CompletionResponse",
+    "EnvironmentSecretSource",
     "MockProvider",
+    "OpenAICompatibleProvider",
     "OrchestrationDecision",
     "ProviderRegistry",
     "ProviderStatus",
