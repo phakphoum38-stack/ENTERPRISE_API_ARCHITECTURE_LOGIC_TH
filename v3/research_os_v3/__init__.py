cbac199e5084809e759d00925776e46b8389c7a1
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-08-18 20:31:47 +0700

    feat(research-os): export network research adapters

--- v3/research_os_v3/__init__.py
+++ v3/research_os_v3/__init__.py
@@ -1,25 +1,12 @@
 from .autonomous_research import AutonomousResearchLoop, AutonomousResearchResult
 from .brain import BrainCore
 from .contracts import health_contract, master_contract, providers_contract
-from .execution import (
-    AtomicExecutionEvidenceStore,
-    FactoryExecutionContext,
-    FactoryExecutionEngine,
-    FactoryExecutionResult,
-    StageEvidence,
-    StageHandler,
-)
+from .execution import AtomicExecutionEvidenceStore, FactoryExecutionContext, FactoryExecutionEngine, FactoryExecutionResult, StageEvidence, StageHandler
 from .factory import SoftwareFactory
 from .models import OrchestrationDecision, ScaleProfile, ScaleTier, Workload
+from .network_research_tools import GitHubResearchTool, WebResearchTool
 from .orchestrator import UnifiedMasterOrchestrator
-from .providers import (
-    CompletionRequest,
-    CompletionResponse,
-    MockProvider,
-    OpenAICompatibleProvider,
-    ProviderRegistry,
-    ProviderStatus,
-)
+from .providers import CompletionRequest, CompletionResponse, MockProvider, OpenAICompatibleProvider, ProviderRegistry, ProviderStatus
 from .queue import DurableTaskQueue, QueueTask
 from .resilience import CircuitBreakerPolicy, CircuitOpenError, ProviderUnavailableError, RetryPolicy
 from .research_checkpoint import ResearchCheckpoint, ResearchCheckpointStore
@@ -34,16 +21,15 @@ from .storage import DataLayout, UserDataLayout
 from .user_context import UserContext, safe_local_user_id
 
 __all__ = [
-    "AtomicExecutionEvidenceStore", "AutonomousResearchLoop", "AutonomousResearchResult", "BrainCore",
-    "BuiltinResearchTools", "CircuitBreakerPolicy", "CircuitOpenError", "CompletionRequest", "CompletionResponse",
-    "CompositeSecretSource", "DataLayout", "DurableTaskQueue", "EnvironmentSecretSource", "FactoryExecutionContext",
-    "FactoryExecutionEngine", "FactoryExecutionResult", "FileResearchTool", "MockProvider", "OpenAICompatibleProvider",
-    "OrchestrationDecision", "PROFILE_HEADER", "ProviderRegistry", "ProviderStatus", "ProviderUnavailableError",
-    "PythonResearchTool", "QueueTask", "ReportFinding", "ResearchCheckpoint", "ResearchCheckpointStore",
-    "ResearchExecutionCoordinator", "ResearchExecutionResult", "ResearchReport", "ResearchReportBuilder", "ResearchTool",
-    "ResearchToolError", "ResearchToolRegistry", "RetryPolicy", "RunnerResult", "ScaleProfile", "ScaleTier",
-    "ShellResearchTool", "SoftwareFactory", "StageEvidence", "StageHandler", "StatelessResearchRunner", "ToolRequest",
-    "ToolResult", "USER_HEADER", "UnifiedMasterOrchestrator", "UserContext", "UserDataLayout", "V3LocalService",
-    "WindowsCredentialManagerSecretSource", "Workload", "default_secret_source", "health_contract", "master_contract",
+    "AtomicExecutionEvidenceStore", "AutonomousResearchLoop", "AutonomousResearchResult", "BrainCore", "BuiltinResearchTools",
+    "CircuitBreakerPolicy", "CircuitOpenError", "CompletionRequest", "CompletionResponse", "CompositeSecretSource", "DataLayout",
+    "DurableTaskQueue", "EnvironmentSecretSource", "FactoryExecutionContext", "FactoryExecutionEngine", "FactoryExecutionResult",
+    "FileResearchTool", "GitHubResearchTool", "MockProvider", "OpenAICompatibleProvider", "OrchestrationDecision", "PROFILE_HEADER",
+    "ProviderRegistry", "ProviderStatus", "ProviderUnavailableError", "PythonResearchTool", "QueueTask", "ReportFinding",
+    "ResearchCheckpoint", "ResearchCheckpointStore", "ResearchExecutionCoordinator", "ResearchExecutionResult", "ResearchReport",
+    "ResearchReportBuilder", "ResearchTool", "ResearchToolError", "ResearchToolRegistry", "RetryPolicy", "RunnerResult",
+    "ScaleProfile", "ScaleTier", "ShellResearchTool", "SoftwareFactory", "StageEvidence", "StageHandler", "StatelessResearchRunner",
+    "ToolRequest", "ToolResult", "USER_HEADER", "UnifiedMasterOrchestrator", "UserContext", "UserDataLayout", "V3LocalService",
+    "WebResearchTool", "WindowsCredentialManagerSecretSource", "Workload", "default_secret_source", "health_contract", "master_contract",
     "providers_contract", "safe_local_user_id",
 ]
