2196a874e079fcf1198c6b1322ecf5ab38e518e6
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-08-18 20:27:18 +0700

    feat(research-os): export autonomous tools and reports

--- v3/research_os_v3/__init__.py
+++ v3/research_os_v3/__init__.py
@@ -1,3 +1,4 @@
+from .autonomous_research import AutonomousResearchLoop, AutonomousResearchResult
 from .brain import BrainCore
 from .contracts import health_contract, master_contract, providers_contract
 from .execution import (
@@ -20,34 +21,27 @@ from .providers import (
     ProviderStatus,
 )
 from .queue import DurableTaskQueue, QueueTask
-from .resilience import (
-    CircuitBreakerPolicy,
-    CircuitOpenError,
-    ProviderUnavailableError,
-    RetryPolicy,
-)
+from .resilience import CircuitBreakerPolicy, CircuitOpenError, ProviderUnavailableError, RetryPolicy
 from .research_checkpoint import ResearchCheckpoint, ResearchCheckpointStore
 from .research_execution import ResearchExecutionCoordinator, ResearchExecutionResult
+from .research_report import ReportFinding, ResearchReport, ResearchReportBuilder
+from .research_tools import ResearchTool, ResearchToolError, ResearchToolRegistry, ToolRequest, ToolResult
 from .runner import RunnerResult, StatelessResearchRunner
-from .secrets import (
-    CompositeSecretSource,
-    EnvironmentSecretSource,
-    WindowsCredentialManagerSecretSource,
-    default_secret_source,
-)
+from .secrets import CompositeSecretSource, EnvironmentSecretSource, WindowsCredentialManagerSecretSource, default_secret_source
 from .service import PROFILE_HEADER, USER_HEADER, V3LocalService
 from .storage import DataLayout, UserDataLayout
 from .user_context import UserContext, safe_local_user_id
 
 __all__ = [
-    "AtomicExecutionEvidenceStore", "BrainCore", "CircuitBreakerPolicy", "CircuitOpenError",
-    "CompletionRequest", "CompletionResponse", "CompositeSecretSource", "DataLayout",
-    "DurableTaskQueue", "EnvironmentSecretSource", "FactoryExecutionContext", "FactoryExecutionEngine",
-    "FactoryExecutionResult", "MockProvider", "OpenAICompatibleProvider", "OrchestrationDecision",
-    "PROFILE_HEADER", "ProviderRegistry", "ProviderStatus", "ProviderUnavailableError", "QueueTask",
+    "AtomicExecutionEvidenceStore", "AutonomousResearchLoop", "AutonomousResearchResult", "BrainCore",
+    "CircuitBreakerPolicy", "CircuitOpenError", "CompletionRequest", "CompletionResponse", "CompositeSecretSource",
+    "DataLayout", "DurableTaskQueue", "EnvironmentSecretSource", "FactoryExecutionContext", "FactoryExecutionEngine",
+    "FactoryExecutionResult", "MockProvider", "OpenAICompatibleProvider", "OrchestrationDecision", "PROFILE_HEADER",
+    "ProviderRegistry", "ProviderStatus", "ProviderUnavailableError", "QueueTask", "ReportFinding",
     "ResearchCheckpoint", "ResearchCheckpointStore", "ResearchExecutionCoordinator", "ResearchExecutionResult",
+    "ResearchReport", "ResearchReportBuilder", "ResearchTool", "ResearchToolError", "ResearchToolRegistry",
     "RetryPolicy", "RunnerResult", "ScaleProfile", "ScaleTier", "SoftwareFactory", "StageEvidence", "StageHandler",
-    "StatelessResearchRunner", "USER_HEADER", "UnifiedMasterOrchestrator", "UserContext", "UserDataLayout",
-    "V3LocalService", "WindowsCredentialManagerSecretSource", "Workload", "default_secret_source",
+    "StatelessResearchRunner", "ToolRequest", "ToolResult", "USER_HEADER", "UnifiedMasterOrchestrator", "UserContext",
+    "UserDataLayout", "V3LocalService", "WindowsCredentialManagerSecretSource", "Workload", "default_secret_source",
     "health_contract", "master_contract", "providers_contract", "safe_local_user_id",
 ]
