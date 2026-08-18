3ee1cbba6a1a94eb78f3a7d39cfb2267a869b6b0
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-08-18 21:01:07 +0700

    feat(research-os): export persistent tool evidence

--- v3/research_os_v3/__init__.py
+++ v3/research_os_v3/__init__.py
@@ -6,6 +6,7 @@ from .factory import SoftwareFactory
 from .models import OrchestrationDecision, ScaleProfile, ScaleTier, Workload
 from .network_research_tools import GitHubResearchTool, WebResearchTool
 from .orchestrator import UnifiedMasterOrchestrator
+from .persistent_tool_evidence import PersistentToolEvidenceSink
 from .providers import CompletionRequest, CompletionResponse, MockProvider, OpenAICompatibleProvider, ProviderRegistry, ProviderStatus
 from .queue import DurableTaskQueue, QueueTask
 from .resilience import CircuitBreakerPolicy, CircuitOpenError, ProviderUnavailableError, RetryPolicy
@@ -17,7 +18,6 @@ from .research_tool_adapters import BuiltinResearchTools, FileResearchTool, Pyth
 from .runner import RunnerResult, StatelessResearchRunner
 from .secrets import CompositeSecretSource, EnvironmentSecretSource, WindowsCredentialManagerSecretSource, default_secret_source
 from .service import PROFILE_HEADER, USER_HEADER, V3LocalService
-from .storage import DataLayout, UserDataLayout
 from .tool_evidence import EvidenceSink, InMemoryEvidenceSink, ToolEvidence, ToolEvidenceRecorder
 from .user_context import UserContext, safe_local_user_id
 
@@ -26,10 +26,10 @@ __all__ = [
     "CircuitBreakerPolicy", "CircuitOpenError", "CompletionRequest", "CompletionResponse", "CompositeSecretSource", "DataLayout",
     "DurableTaskQueue", "EnvironmentSecretSource", "EvidenceSink", "FactoryExecutionContext", "FactoryExecutionEngine", "FactoryExecutionResult",
     "FileResearchTool", "GitHubResearchTool", "InMemoryEvidenceSink", "MockProvider", "OpenAICompatibleProvider", "OrchestrationDecision", "PROFILE_HEADER",
-    "ProviderRegistry", "ProviderStatus", "ProviderUnavailableError", "PythonResearchTool", "QueueTask", "ReportFinding", "ResearchCheckpoint",
-    "ResearchCheckpointStore", "ResearchExecutionCoordinator", "ResearchExecutionResult", "ResearchReport", "ResearchReportBuilder", "ResearchTool",
-    "ResearchToolError", "ResearchToolRegistry", "RetryPolicy", "RunnerResult", "ScaleProfile", "ScaleTier", "ShellResearchTool", "SoftwareFactory",
-    "StageEvidence", "StageHandler", "StatelessResearchRunner", "ToolEvidence", "ToolEvidenceRecorder", "ToolRequest", "ToolResult", "USER_HEADER",
-    "UnifiedMasterOrchestrator", "UserContext", "UserDataLayout", "V3LocalService", "WebResearchTool", "WindowsCredentialManagerSecretSource", "Workload",
-    "default_secret_source", "health_contract", "master_contract", "providers_contract", "safe_local_user_id",
+    "PersistentToolEvidenceSink", "ProviderRegistry", "ProviderStatus", "ProviderUnavailableError", "PythonResearchTool", "QueueTask", "ReportFinding",
+    "ResearchCheckpoint", "ResearchCheckpointStore", "ResearchExecutionCoordinator", "ResearchExecutionResult", "ResearchReport", "ResearchReportBuilder",
+    "ResearchTool", "ResearchToolError", "ResearchToolRegistry", "RetryPolicy", "RunnerResult", "ScaleProfile", "ScaleTier", "ShellResearchTool",
+    "SoftwareFactory", "StageEvidence", "StageHandler", "StatelessResearchRunner", "ToolEvidence", "ToolEvidenceRecorder", "ToolRequest", "ToolResult",
+    "USER_HEADER", "UnifiedMasterOrchestrator", "UserContext", "UserDataLayout", "V3LocalService", "WebResearchTool", "WindowsCredentialManagerSecretSource",
+    "Workload", "default_secret_source", "health_contract", "master_contract", "providers_contract", "safe_local_user_id",
 ]
