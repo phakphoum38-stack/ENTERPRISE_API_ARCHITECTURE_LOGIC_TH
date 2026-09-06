0ae31c8dd9eeb5a119f08783e9057a5f84e99edf
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-09-07 04:58:23 +0700

    feat(friend): export Mission Control capability projection

--- owner_special/research_os_friend/__init__.py
+++ owner_special/research_os_friend/__init__.py
@@ -14,6 +14,7 @@ from .identity import OwnerIdentity
 from .memory import ScopedMemory
 from .models import FriendDecision, FriendRequest, FriendResponse, ScaleProfile
 from .mission_control import MissionControl
+from .mission_control_capabilities import MissionControlCapabilities
 from .orchestrator import FriendOrchestrator
 from .persistent_memory import PersistentScopedMemory
 from .policy import OwnerPolicy
@@ -31,7 +32,7 @@ from .v3_bridge import V3Bridge, V3BridgeStatus
 __all__ = [
     "AgentRun", "AgentRuntime", "AgentRunStatus", "AgentTraceEvent", "PersistentAgentTraceStore", "AgentsSdkAdapter", "AgentsSdkContract", "AgentsSdkToolContract", "ApprovalGate", "ApprovalRecord", "ApprovalState", "SIDE_EFFECT_TOOLS", "PersistentApprovalStore", "Capability", "CapabilityRegistry", "DecisionPlanner", "EvidenceRecorder", "FriendBrain", "FriendContext",
     "FriendDecision", "FriendOrchestrator", "FriendRequest", "FriendResponse", "FriendRuntime", "HelperAllocation",
-    "HelperScheduler", "MemorySecretStore", "MissionControl", "MockProvider", "OpenAICompatibleProvider", "OWNER_HEADER", "OwnerBundleBuilder",
+    "HelperScheduler", "MemorySecretStore", "MissionControl", "MissionControlCapabilities", "MockProvider", "OpenAICompatibleProvider", "OWNER_HEADER", "OwnerBundleBuilder",
     "OwnerFriendService", "OwnerIdentity", "OwnerPolicy", "PROFILE_HEADER", "PersistentScopedMemory", "ProviderManager",
     "ProviderRouter", "SESSION_HEADER", "ScaleProfile", "ScopedMemory", "Skill", "SkillRegistry", "Tool", "ToolRegistry",
     "ToolSearch", "ToolSearchResult", "TOOL_CATALOG", "ToolDescriptor", "ToolState", "UnifiedToolCatalog", "V3Bridge", "V3BridgeStatus",
