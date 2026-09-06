185412bfe0dc493f7a96d3522e2821b9be297b25
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-09-06 19:55:42 +0700

    feat(friend): export approval state API

--- owner_special/research_os_friend/__init__.py
+++ owner_special/research_os_friend/__init__.py
@@ -1,5 +1,6 @@
 from .agent_runtime import AgentRun, AgentRuntime, AgentRunStatus, AgentTraceEvent
 from .agent_trace_store import PersistentAgentTraceStore
+from .approval import ApprovalGate, ApprovalRecord, ApprovalState, SIDE_EFFECT_TOOLS
 from .brain import FriendBrain
 from .bundle import OwnerBundleBuilder
 from .capabilities import Capability, CapabilityRegistry, install_friend_complete_capabilities
@@ -25,7 +26,7 @@ from .unified_tool_catalog import TOOL_CATALOG, ToolDescriptor, ToolState, Unifi
 from .v3_bridge import V3Bridge, V3BridgeStatus
 
 __all__ = [
-    "AgentRun", "AgentRuntime", "AgentRunStatus", "AgentTraceEvent", "PersistentAgentTraceStore", "Capability", "CapabilityRegistry", "DecisionPlanner", "EvidenceRecorder", "FriendBrain", "FriendContext",
+    "AgentRun", "AgentRuntime", "AgentRunStatus", "AgentTraceEvent", "PersistentAgentTraceStore", "ApprovalGate", "ApprovalRecord", "ApprovalState", "SIDE_EFFECT_TOOLS", "Capability", "CapabilityRegistry", "DecisionPlanner", "EvidenceRecorder", "FriendBrain", "FriendContext",
     "FriendDecision", "FriendOrchestrator", "FriendRequest", "FriendResponse", "FriendRuntime", "HelperAllocation",
     "HelperScheduler", "MemorySecretStore", "MockProvider", "OpenAICompatibleProvider", "OWNER_HEADER", "OwnerBundleBuilder",
     "OwnerFriendService", "OwnerIdentity", "OwnerPolicy", "PROFILE_HEADER", "PersistentScopedMemory", "ProviderManager",
