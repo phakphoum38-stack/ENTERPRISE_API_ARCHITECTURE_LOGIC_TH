63437771cfac6751a459bfce65e57765b4664949
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-09-06 18:42:45 +0700

    feat(friend): expose self-learning v2 primitives

--- owner_special/research_os_friend/self_learning/__init__.py
+++ owner_special/research_os_friend/self_learning/__init__.py
@@ -1,5 +1,14 @@
+from .feedback import SkillFeedback
 from .learner import SelfLearningEngine
 from .models import LearnedSkillCandidate
 from .registry import LearnedSkillRegistry
+from .versioning import SkillVersionProposal, propose_next_version
 
-__all__ = ["LearnedSkillCandidate", "LearnedSkillRegistry", "SelfLearningEngine"]
+__all__ = [
+    "LearnedSkillCandidate",
+    "LearnedSkillRegistry",
+    "SelfLearningEngine",
+    "SkillFeedback",
+    "SkillVersionProposal",
+    "propose_next_version",
+]
