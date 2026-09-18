30fb07247d5fbac8e1c9f236b3805e59eed10bde
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-09-19 01:23:30 +0700

    fix(control): make Control Center the default shell surface

--- owner_special/flutter_app/lib/src/friend_app.dart
+++ owner_special/flutter_app/lib/src/friend_app.dart
@@ -21,7 +21,7 @@ class OwnerFriendApp extends StatefulWidget {
 }
 
 class _OwnerFriendAppState extends State<OwnerFriendApp> {
-  int _index = 1;
+  int _index = 0;
   TeamRecord _currentTeam = const TeamRecord(id: 'research', name: 'Research Team');
 
   void _onTeamChanged(TeamRecord team) => setState(() => _currentTeam = team);
