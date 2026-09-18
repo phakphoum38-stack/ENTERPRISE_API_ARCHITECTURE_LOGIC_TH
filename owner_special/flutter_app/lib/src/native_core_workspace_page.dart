43cd27c4a18acf3fc904d010c07d665a73e6d57f
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-09-19 03:34:42 +0700

    style(flutter): use interpolation in workspace event

--- owner_special/flutter_app/lib/src/native_core_workspace_page.dart
+++ owner_special/flutter_app/lib/src/native_core_workspace_page.dart
@@ -84,7 +84,7 @@ class _NativeCoreWorkspacePageState extends State<NativeCoreWorkspacePage>
     final route = routes[command];
     if (route != null) {
       widget.onNavigate?.call(route);
-      setState(() => _events.insert(0, command + ' prepared'));
+      setState(() => _events.insert(0, '$command prepared'));
       return;
     }
     if (command == 'Inspect object') _tabs.animateTo(4);
