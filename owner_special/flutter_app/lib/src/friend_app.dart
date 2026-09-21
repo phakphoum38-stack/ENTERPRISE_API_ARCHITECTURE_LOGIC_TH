dd944635bbabad3e2e7816c0584bc6cb036b799b
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-09-21 12:29:50 +0700

    reconcile: carry PR #500 payload owner_special/flutter_app/lib/src/friend_app.dart

--- owner_special/flutter_app/lib/src/friend_app.dart
+++ owner_special/flutter_app/lib/src/friend_app.dart
@@ -6,6 +6,7 @@ import 'contracts/owner_friend_capability_adapter.dart';
 import 'contracts/owner_research_capability_adapter.dart';
 import 'package:research_os_contracts/research_os_contracts.dart';
 import 'friend_module_shell.dart';
+import 'friend_connections_page.dart';
 import 'friend_theme.dart';
 import 'google_identity_page.dart';
 import 'launch_desk_page.dart';
@@ -47,6 +48,7 @@ class _OwnerFriendAppState extends State<OwnerFriendApp> {
       _ProviderPage(api: widget.api),
       _TeamPage(team: _currentTeam),
       GoogleIdentityPage(api: widget.api),
+      FriendConnectionsPage(api: widget.api),
     ];
 
     return MaterialApp(
