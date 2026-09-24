02103e27e7ef3aabb448ee4ab3a9e69cc193ff9f
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-09-24 15:32:22 +0700

    test(research-os): cover 10 20 50 100 project readiness

--- tools/test_platform_core_completion.py
+++ tools/test_platform_core_completion.py
@@ -12,6 +12,7 @@ class PlatformCoreCompletionTests(unittest.TestCase):
         self.assertEqual(100, len(contexts))
         self.assertEqual((), project_scale_readiness.validate_project_contexts(contexts))
         self.assertEqual((), project_scale_readiness.validate_registry_scale())
+        self.assertEqual((), project_scale_readiness.validate_scale_levels())
     def test_project_isolation(self) -> None:
         contexts = project_scale_readiness.build_project_contexts()
         self.assertEqual((), project_scale_readiness.validate_isolation(project_a=contexts[0], project_b=contexts[1]))
